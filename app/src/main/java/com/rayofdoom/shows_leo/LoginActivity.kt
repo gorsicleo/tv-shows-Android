bf79394aa8be599d5d85f3dac78996244a6f379c
Leo Goršić <gorsicleo@gmail.com>
2021-07-13 04:12:39 +0200

    Improved code readability, added transparent status bar and improved design (corrections regarding button height, textbox hint color when selected...), code is decluttered and most of pull request code changes have been taken into account. Ready for another pull req.

--- app/src/main/java/com/rayofdoom/shows_leo/LoginActivity.kt
+++ app/src/main/java/com/rayofdoom/shows_leo/LoginActivity.kt
@@ -7,100 +7,24 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import com.rayofdoom.shows_leo.databinding.ActivityLoginBinding
+import com.rayofdoom.shows_leo.util_functions.textListenersInit
 
-
-var conditionEmail = false
-var conditionPass = false
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
+    var conditionEmail = false
+    var conditionPass = false
+
+    lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.loginButton.isClickable = false //BUTTON SHOULD BE DISABLED AT START
-        binding.emailInput.addTextChangedListener(object :TextWatcher{
-            override fun afterTextChanged(s: Editable?) {
-                val inputString = binding.emailInput.text
-                validateEmail(inputString.toString())
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            }
-
-        })
-
-        binding.passwordInput.addTextChangedListener(object :TextWatcher{
-            override fun afterTextChanged(s: Editable?) {
-                val inputString = binding.passwordInput.text
-                validatePass(inputString.toString())
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            }
-
-        })
-
-
-
+        //this is used to disable button when app starts
+        binding.loginButton.isEnabled = false
+        textListenersInit()
     }
 
 
-
-    private fun validateEmail(inputString: String){
-        if (!inputString.contains("@")) setErrorEmail("Email address is not valid because @ is missing!")
-        else if(inputString.length < 2) setErrorEmail("Email must have at least 2 characters!")
-        else {
-            conditionEmail=true
-            enableButton()
-        }
-
-    }
-
-    private fun validatePass(inputString: String){
-        if (inputString.length<6) setErrorPass("Password is weak. Minimum of 6 characters")
-        else if (!inputString.matches(Regex(".*\\d.*"))) setErrorPass("Password should contain number") //NOT WORKING!!
-        else {
-            conditionPass=true
-            enableButton()
-        }
-    }
-
-    private fun setErrorEmail(msg: String){
-        conditionEmail=false
-        binding.loginButton.isClickable = false
-        binding.loginButton.setBackgroundColor(Color.parseColor("#4EBBBBBB"))
-        binding.emailInput.error = msg
-
-    }
-
-    private fun setErrorPass(msg: String){
-        conditionPass = false
-        binding.loginButton.isClickable = false
-        binding.loginButton.setBackgroundColor(Color.parseColor("#4EBBBBBB"))
-        binding.passwordInput.error = msg
-
-    }
-    private fun enableButton(){
-        if (conditionEmail && conditionPass){
-        binding.loginButton.isClickable=true
-        binding.loginButton.setBackgroundColor(Color.WHITE)
-        binding.loginButton.setTextColor(Color.parseColor("#52368C"))
-        }
-        binding.loginButton.setOnClickListener{
-            //val alertDialogBuilder = AlertDialog.Builder(this) //
-            //alertDialogBuilder.setMessage("USPJEH").show() //USED FOR TESTING PURPOSES
-            intent = Intent(this, WelcomeActivity::class.java)
-            intent.putExtra("user",binding.emailInput.text )
-            startActivity(intent)
-
-        }
-    }
 }
--- app/src/main/java/com/rayofdoom/shows_leo/WelcomeActivity.kt
+++ app/src/main/java/com/rayofdoom/shows_leo/WelcomeActivity.kt
@@ -2,10 +2,12 @@ package com.rayofdoom.shows_leo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.content.Intent
 import com.rayofdoom.shows_leo.databinding.ActivityWelcomeBinding
 
+private const val EMAIL_USERNAME_SEPARATOR = "@"
+
 class WelcomeActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityWelcomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +15,17 @@ class WelcomeActivity : AppCompatActivity() {
         supportActionBar?.hide()
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val bundle: Bundle? = intent.extras
-        val bundleget = bundle?.get("user")
-        val welcomeStr = bundleget.toString().subSequence(0,bundleget.toString().indexOf("@"))
-        binding.welcomeMsg.text = "Welcome, "+welcomeStr
 
+        displayWelcomeMessage()
+    }
 
+    private fun displayWelcomeMessage() {
+        val welcomeMessage = intent.getStringExtra("user").toString()
+            .subSequence(
+                0,
+                intent.getStringExtra("user").toString().indexOf(EMAIL_USERNAME_SEPARATOR)
+            )
+
+        binding.welcomeMessage.text = String.format("Welcome, %s", welcomeMessage)
     }
 }
